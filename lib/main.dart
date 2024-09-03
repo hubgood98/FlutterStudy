9bcbfd4751bb01f6af3771465878b08299163f7a
hubgood98 <heej1998@gmail.com>
2024-09-04 01:38:20 +0900

    배치 공부

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(RowWidget());
 }
 
 class MyApp extends StatelessWidget{
@@ -11,15 +11,106 @@ class MyApp extends StatelessWidget{
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: TextButton(
+        body: Container(
+          color: Colors.green,
+          child:
+          Center(
+            child: TextButton(
               onPressed: (){},
               style: TextButton.styleFrom(
                 foregroundColor: Colors.red,
               ), child: const Text('텍스트 버튼'),
             ),
           ),
+        )
       ),
     );
   }
+}
+
+class RowWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SizedBox(
+          height: double.infinity,
+          child: Row(
+            //주축 정렬 지정
+            mainAxisAlignment: MainAxisAlignment.start,
+            //반대축 정렬 지정
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              Container(
+                height: 50,
+                width: 50,
+                color: Colors.lime,
+            ),
+
+              //sizebox는 일반적으로 공백생성할 때 사용
+              const SizedBox(width:12.0),
+              Container(
+                height: 50.0,
+                width: 50.0,
+                color: Colors.blueAccent,
+              ),
+
+              const SizedBox(width: 12.0),
+              Container(
+                height: 80,
+                width: 80,
+                color: Colors.amber,
+              )
+            ],
+          ),
+        ),
+      )
+    );
+  }
+
+}
+
+class ColumnWidget extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+          body: SizedBox(
+            height: double.infinity,
+            child: Column(
+              //주축 정렬 지정
+              mainAxisAlignment: MainAxisAlignment.start,
+              //반대축 정렬 지정
+              crossAxisAlignment: CrossAxisAlignment.center,
+
+              children: [
+                Container(
+                  height: 50,
+                  width: 50,
+                  color: Colors.lime,
+                ),
+
+                //sizebox는 일반적으로 공백생성할 때 사용
+                const SizedBox(width:12.0),
+                Container(
+                  height: 50.0,
+                  width: 50.0,
+                  color: Colors.blueAccent,
+                ),
+
+                const SizedBox(width: 12.0),
+                Container(
+                  height: 80,
+                  width: 80,
+                  color: Colors.amber,
+                )
+              ],
+            ),
+          ),
+        )
+    );
+  }
+
+
 }
