9f14da6bb1617b64f8b0e748079898b69b2641e8
hubgood98 <heej1998@gmail.com>
2024-09-05 23:59:56 +0900

    슬라이더 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,21 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:async';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+
+  final PageController pageController = PageController();
+
+  void initState()
+  {
+    super.initState();
+
+    Timer.periodic(
+      Duration(seconds: 3),
+        (timer){
+          //현재 페이지 가져오기
+          int? nextPage = pageController.page?.toInt();
+
+          if(nextPage == null)
+            {
+              return;
+            }
+
+          if(nextPage > 2)
+            {
+              nextPage = 0;
+            }else{
+            nextPage++;
+          }
+          pageController.animateToPage(
+              nextPage,
+              duration: Duration(milliseconds: 500),
+              curve: Curves.ease,
+          );
+        },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body:
-        Padding(padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-          Text('홈 화면',style: TextStyle(fontSize: 15)),
-          Text('두번째 텍스트'),
-          ],
-        )
-       ),
-      );
+
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        children: [1,2,3]
+            .map(
+              (number) => Image.asset(
+                  'assets/sea$number.jpg',
+                  fit: BoxFit.cover),
+        ).toList()
+      )
+    );
   }
 
 }
