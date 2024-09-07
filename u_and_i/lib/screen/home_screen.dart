f8f54be00630e4d6075b8a5b3ac9717be5f11cf3
hubgood98 <heej1998@gmail.com>
2024-09-07 23:13:01 +0900

    extends 오타수정

--- u_and_i/lib/screen/home_screen.dart
+++ u_and_i/lib/screen/home_screen.dart
@@ -19,7 +19,7 @@ class _DDay extends StatelessWidget{
   }
 }
 
-class _CoupleImage extends StatelㅇessWidget{
+class _CoupleImage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Text('커플 이미지 위젯');
